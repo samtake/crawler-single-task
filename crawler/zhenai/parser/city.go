e471ec5530d323c00e557a488492447137ff6006
huanglongshan689 <943430195@qq.com>
2019-12-08 16:42:26 +0800

    111

--- crawler/zhenai/parser/city.go
+++ crawler/zhenai/parser/city.go
@@ -13,13 +13,15 @@ func ParserCity(contents []byte) engine.ParseResult {
 
 	result := engine.ParseResult{}
 	for _, m := range matches {
+		name:=string(m[2])
 		result.Items = append(result.Items, "User "+string(m[2]))
 
 		result.Requests = append(result.Requests, engine.Request{
 			Url: string(m[1]),
 			// ParserFunc: engine.NilParser,
 			ParserFunc: func(contents []byte) engine.ParseResult {
-				return ParseProfile(contents, string(m[2]))
+				// return ParseProfile(contents, string(m[2]))
+				return ParseProfile(contents, name)
 			},
 		})
 	}
