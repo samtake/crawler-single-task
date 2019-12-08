a6199a6e561a59bbd6186cd00ad24bc044531e21
huanglongshan689 <943430195@qq.com>
2019-12-08 13:47:55 +0800

    用户信息解析器

--- crawler/zhenai/parser/city.go
+++ crawler/zhenai/parser/city.go
@@ -1,24 +1,26 @@
 package parser
 
 import (
-	"regexp"
 	"crawler-single-task/engine"
+	"regexp"
 )
 
 const cityRe = `<a href="(http://album.zhenai.com/u/[0-9]+)"[^>]*>([^<]+)</a>`
 
 func ParserCity(contents []byte) engine.ParseResult {
-	re:= regexp.MustCompile(cityRe)
+	re := regexp.MustCompile(cityRe)
 	matches := re.FindAllSubmatch(contents, -1)
 
 	result := engine.ParseResult{}
 	for _, m := range matches {
-		result.Items = append(result.Items, "User" +string(m[2]))
+		result.Items = append(result.Items, "User "+string(m[2]))
 
-
-		result.Requests=append(result.Requests,engine.Request{
+		result.Requests = append(result.Requests, engine.Request{
 			Url: string(m[1]),
-			ParserFunc: engine.NilParser,
+			// ParserFunc: engine.NilParser,
+			ParserFunc: func(contents []byte) engine.ParseResult {
+				return ParseProfile(contents, string(m[2]))
+			},
 		})
 	}
 	return result
